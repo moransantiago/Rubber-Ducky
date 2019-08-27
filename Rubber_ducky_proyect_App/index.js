aadac7443c5e2b97ad0e4560be496424862e5792
Santiago Moran <santimoran.1@hotmail.com>
2019-08-27 01:03:11 -0300

    Socket emition ready

--- Rubber_ducky_proyect_App/index.js
+++ Rubber_ducky_proyect_App/index.js
@@ -1,44 +1,10 @@
-const path = require('path');
-const express = require('express');
-const app = express();
+const net = require('net');
 
-// settings
-app.set('port', process.env.PORT || 3000);
-
-// static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get("/", (req, res, next) => {
-//     res.send('./');
-// });
-
-// start the server
-const server = app.listen(app.get('port'), () => {
-    console.log('server on port', app.get('port'));
-});
-
-// websockets
-const SocketIO = require('socket.io');
-const io = SocketIO(server);
-
-io.on('connection', socket => {
-    console.log('new connection', socket.id);
-
-    socket.on('chat:message', data => {
-        console.log(data);
+const server = net.createServer(socket => {
+    console.log("Client connected!");
+    socket.on('data', data => {
+        console.log(data.toString());
     })
 });
 
-// const net = require('net');
-// const server = net.createServer(socket => {
-//     socket.write(
-//         "Testing !" +
-//             socket.remoteAddress +
-//             ":" +
-//             socket.remotePort +
-//             "\n"
-//     );
-//     socket.pipe(socket);
-//     console.log("Client connected!");
-// });
-// server.listen(3000, "localhost");
+server.listen(2001, "localhost");
--- Rubber_ducky_proyect_App/public/test.js
+++ Rubber_ducky_proyect_App/public/test.js
@@ -2,7 +2,7 @@ const socket = io();
 const btn = document.getElementById('send');
 const message = document.getElementById('inputMessage');
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', _ => {
     socket.emit('chat:message', {
         message: message.value
     });
